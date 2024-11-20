d9238705ac32ecc8fb35c2741efaceb09f821911
Sebastián Torres <sebastian.torres@ielamilagrosabello.edu.co>
2024-11-19 23:51:08 -0500

    Subiendo datos a la rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Card from './components/Card';
+import Counter from './components/counter';
 
 function App() {
   return (
     <div>
       <h1>Mi Proyecto con React y Vite</h1>
       <Card title="Hola Mundo" description="Este es un ejemplo de componente en React." />
+      <Counter />
     </div>
   );
 }
 
 export default App;
-
